192ab2a40ba29b0c78bbce28edd11c0d204ba905
Raúl Miñán <raulminan@gmail.com>
2022-07-26 22:36:41 +0200

    feat: add test cases for problem BA1K

--- tests/test_BA1K.py
+++ tests/test_BA1K.py
@@ -1,13 +1,35 @@
 import unittest
 from exercises import BA1K
 
-class Tests(unittest.TestCase):
+class Test:
     def test_computing_frequencies(self):
+        """Test general case"""
         text = "ACGCGGCTCTGAAA"
         k = 2
-        result = "2 1 0 0 0 0 2 2 1 2 1 0 0 1 1 0"
+        correct_output = "2 1 0 0 0 0 2 2 1 2 1 0 0 1 1 0"
+        result = BA1K.computing_frequencies(text, k, output_str=True)
+        assert result == correct_output
         
-        self.assertEqual(BA1K.computing_frequencies(text, k, output_str=True), result)
+    def test_computing_frequencies_2(self):
+        """Tests if theres off-by-one error at the end of text"""
+        text = "AAAAC"
+        k = 2
+        correct_output = "3 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0"
+        result = BA1K.computing_frequencies(text, k, output_str=True)
+        assert result == correct_output
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_computing_frequencies_3(self):
+        """Tets if theres and off-by-one error at the beggining of text"""
+        text = "TTAAA"
+        k = 2
+        correct_output = "2 0 0 0 0 0 0 0 0 0 0 0 1 0 0 1"
+        result = BA1K.computing_frequencies(text, k, output_str=True)
+        assert result == correct_output
+
+    def test_computing_frequencies_4(self):
+        """Tests if the code is actually updating the count"""
+        text = "AAA"
+        k = 2
+        correct_output = "2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"
+        result = BA1K.computing_frequencies(text, k, output_str=True)
+        assert result == correct_output
